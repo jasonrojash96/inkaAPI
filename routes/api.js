5d0e983068e921a4ff49839bf58e90ab0098c138
Jason Rojas <jrojash96@gmail.com>
2018-06-26 17:40:43 -0500

    add limit and sort

--- routes/api.js
+++ routes/api.js
@@ -8,19 +8,20 @@ var db = mongojs(
 );
 
 router.get("/ranking", (req, res) => {
-  db.ranking.find((err, docs) => {
+  db.ranking.find({}).sort({puntaje:1}).limit(5).skip(1).toArray((err, docs) => {
     res.status(200).send(docs);
   });
 });
 
 router.post("/ranking", (req, res) => {
+  console.log(req.body)
   if (!req.body.puntaje || !req.body.nombre) {
     res.status(400).send({ code: 1, msg: "Parametros invalidos" });
     return;
   }
   db.ranking.insert({
     nombre: req.body.nombre,
-    puntaje: req.body.puntaje
+    puntaje: Number(req.body.puntaje)
   });
   res.status(200).send({code : 0 , msg : "Registro válido"});
 });
